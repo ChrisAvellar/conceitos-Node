c2ae5f6b40b15a1988ff5220728abe6d9c0e9249
ChrisAvellar <2kavellar@gmail.com>
2024-06-18 13:45:41 -0300

    NODE DATABASE

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/usuarios', (req, res)=>{
+    res.send("Olá, rota acessada com sucesso!")
+})
+
+app.post('/usuarios', (req, res)=>{
+    res.send("Olá")
+})
+
+
+
+
+app.listen(3000)
